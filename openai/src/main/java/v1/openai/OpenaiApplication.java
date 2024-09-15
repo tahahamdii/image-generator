ea436306425a0f9aba140564979a39daa8f1b7e1
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-15 16:30:17 +0200

    setuip

--- openai/src/main/java/v1/openai/OpenaiApplication.java
+++ openai/src/main/java/v1/openai/OpenaiApplication.java
@@ -1,7 +1,9 @@
 package v1.openai;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenaiApplication {
@@ -10,4 +12,9 @@ public class OpenaiApplication {
 		SpringApplication.run(OpenaiApplication.class, args);
 	}
 
+
+	@Bean
+	public Dotenv dotenv() {
+		return Dotenv.load();
+	}
 }
