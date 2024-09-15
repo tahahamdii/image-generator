e03b6a4c4836459bd7377bfc72c85cf2bef2a2c8
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-15 17:14:33 +0200

    controller the service

--- openai/src/main/java/v1/openai/Controller.java
+++ openai/src/main/java/v1/openai/Controller.java
@@ -1,18 +1,24 @@
 package v1.openai;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 
 public class Controller {
     ChatService chatService;
+    private final ImageService imageService;
 
-    public Controller(ChatService chatService) {
+    public Controller(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("/query")
@@ -22,5 +28,21 @@ public class Controller {
     @GetMapping("/query2")
     public String getResponse2(@RequestParam String prompt){
         return chatService.getResponseOptions(prompt);
+    }
+
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(HttpServletResponse response,
+                                       @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "1") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt, quality, n, width, height);
+
+        // Streams to get urls from ImageResponse
+        return imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
     }
 }
