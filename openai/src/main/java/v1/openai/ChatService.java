4b33cd0d2359c854354fe4c67531894832b8ad7f
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-15 16:34:08 +0200

    chat service

---
+++ openai/src/main/java/v1/openai/ChatService.java
@@ -0,0 +1,16 @@
+package v1.openai;
+
+import org.springframework.ai.chat.model.ChatModel;
+
+public class ChatService {
+
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel ) {
+        this.chatModel = chatModel;
+    }
+
+    public  String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+}
